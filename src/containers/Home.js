646f934d7c25393915618f6aa08bacbbb3fad055
Oladele Seyi <toothy61@gmail.com>
2020-07-02 23:14:12 +0100

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
@@ -57,8 +58,16 @@ export default function Home() {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>jotIt</h1>
+        <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
